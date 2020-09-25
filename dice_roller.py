8d743437e3f43f7550c43b9ea257017716a2b3ec
Nikhil Chunduri <chundurin@gmail.com>
2020-09-25 12:23:12 +0530

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,19 @@
 import random
 
 def main():
-    dice_rolls=2
+    dice_rolls=int(input("Enter the no of dice"))
+    dice_size=int(input("Enter the no of faces"))
+
     dice_sum=0
     for i in range(0,dice_rolls):
-        roll=random.randint(1,6)
+        roll=random.randint(1,dice_size)
         dice_sum+=roll
-        print(f'You rolled a {roll}')
+        if roll == 1:
+            print(f'You rolled a {roll}! Critical Fail')
+        elif roll == dice_size:
+            print(f'You rolled a {roll}! Critical Success!')
+        else:
+            print(f'You rolled a {roll}')
     print(f'You have rolled a total of {dice_sum}')
 
 
